255df873ab7853aee3684c6d4c39e86fb82ce7d1
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-04-18 20:05:46 +0100

    compute shader ants test

--- Nova/Nova/res/shader/compute.glsl
+++ Nova/Nova/res/shader/compute.glsl
@@ -8,11 +8,9 @@
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 layout(rgba32f, binding = 0) uniform image2D output_image;
 layout(binding = 1) buffer test {
-	float pray[100];
 	vec3 lol;
+	float pray[200];
 };
-
-// imageStore(output_image, pixel_coords, pixel); //vec4(gl_GlobalInvocationID.xy, 0.0, 1.0)
 
 void main() {
 	vec4 p_colour = vec4(lol, 1.0);
