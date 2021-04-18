4984a969f7f467ceb593b50660b1d562ba6cbb36
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-04-18 19:57:49 +0100

    Maths lib

--- Nova/phys/maths.h
+++ Nova/phys/maths.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef NOVA
+#ifndef NOVA_EXPORT
 #include "dep/glm/include/glm/fwd.hpp"
 #include "dep/glm/include/glm/glm.hpp"
 #include "dep/glm/include/glm/ext.hpp"
@@ -8,7 +8,7 @@
 #include <glm/fwd.hpp>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-#endif NOVA
+#endif // NOVA_EXPORT
 
 namespace Nova::mlb {
 	using namespace glm;
