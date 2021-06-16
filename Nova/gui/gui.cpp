2d945a496bb983925d98db54ca3c1da446a7ac74
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-16 15:23:03 +0100

    set gui styling

--- Nova/gui/gui.cpp
+++ Nova/gui/gui.cpp
@@ -31,8 +31,46 @@ namespace Nova::gui {
 
 		io.IniFilename = "Nova/gui.ini";
 
-		auto font = io.Fonts->AddFontFromFileTTF("Nova/font/CascadiaCode_VTT.ttf", 17.0f);
+		auto font = io.Fonts->AddFontFromFileTTF("Nova/font/CascadiaCode_VTT.ttf", 17.0f); // 24px for large
 		io.FontDefault = font;
+
+		auto& style = ImGui::GetStyle();
+
+		// Padding
+		style.WindowPadding = { 4.0, 2.0 };
+		style.FramePadding = { 4.0, 2.0 };
+		style.CellPadding = { 4.0, 2.0 };
+		style.ItemSpacing = { 4.0, 4.0 };
+		style.ItemInnerSpacing = { 4.0, 4.0 };
+		style.TouchExtraPadding = { 0.0, 0.0 };
+
+		style.IndentSpacing = 16;
+		style.ScrollbarSize = 16;
+		style.GrabMinSize = 12;
+
+		// Boarders
+		style.WindowBorderSize = 1;
+		style.ChildBorderSize = 0;
+		style.PopupBorderSize = 0;
+		style.FrameBorderSize = 0;
+		style.TabBorderSize = 0;
+
+		// Rounding
+		style.WindowRounding = 8;
+		style.ChildRounding = 8;
+		style.FrameRounding = 8;
+		style.PopupRounding = 8;
+		style.ScrollbarRounding = 8;
+		style.GrabRounding = 8;
+		style.LogSliderDeadzone = 4;
+		style.TabRounding = 4;
+
+		// Alignment
+		style.WindowTitleAlign = { 0.0, 0.0 };
+		style.WindowMenuButtonPosition = ImGuiDir_Right;
+		style.ColorButtonPosition = ImGuiDir_Right;
+
+
 	}
 
 }
