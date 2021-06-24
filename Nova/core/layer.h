67be529f3bfc99260185fda207928862410a3788
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-24 21:21:27 +0100

    Core Layer rewrite - Not used

--- Nova/core/layer.h
+++ Nova/core/layer.h
@@ -2,15 +2,35 @@
 #include "npch.h"
 #include "event/event.h"
 
-namespace Nova {
+namespace Nova::Layer {
 
-	class Layer {
+	class Item {
 	public:
-		Layer() {}
-		virtual ~Layer() {}
+		Item() {}
+		virtual ~Item() {}
 
 		virtual void update() = 0;
 		virtual void event(Event::Event& event) = 0;
+	};
+
+	class Stack {
+	public:
+		Stack() {}
+
+		void update() {
+			for (auto& it : m_stack) {
+				it->update();
+			}
+		}
+		void event(Event::Event& event) {
+			for (auto it = m_stack.rbegin(); it < m_stack.crend(); ++it) {
+				if (event.done)
+					break;
+				(*it)->event(event);
+			}
+		}
+	protected:
+		std::vector<Item*> m_stack;
 	};
 
 }
