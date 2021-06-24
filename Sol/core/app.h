00c3e7d669d75e310b4cb4888511ca031227a04a
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-24 21:40:50 +0100

    Sol app and main moved

---
+++ Sol/core/app.h
@@ -0,0 +1,23 @@
+#include "spch.h"
+#include <nova.h>
+#include "panel.h"
+#include "editor/camera.h"
+
+namespace Sol {
+	class App : public Nova::Application {
+	public:
+		App();
+		~App() {}
+		virtual void update() override;
+		virtual void event(Nova::Event::Event& event) override;
+
+	public:
+		Nova::Render::Camera* camera;
+		Nova::Star<Nova::gui::App> gui;
+		Nova::Star<Nova::ecs::World> world;
+	private:
+		Sol::Editor::Camera camera_controller;
+		// gui
+		std::unordered_map<std::string, Panel::Panel*> m_panels;
+	};
+}
