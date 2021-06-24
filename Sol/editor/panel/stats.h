040167d52e4dd70ad2d1a0e4351aa08e48d86ab1
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-24 21:39:14 +0100

    Panel: Stats

---
+++ Sol/editor/panel/stats.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "spch.h"
+#include <nova.h>
+#include "core/panel.h"
+
+namespace Sol::Panel {
+	class Stats : public Panel {
+	public:
+		Stats() : Panel("Stats") {}
+		void state(const std::string& state) { m_state = state; }
+	protected:
+		std::string m_state = "Nominal";
+		virtual void gui() override {
+			Nova::gui::Text("CPU Time: %f", Nova::DeltaTime::dt() * 1000.0f);
+			Nova::gui::Text("State: %s", m_state.c_str());
+			m_state = "Nominal";
+		}
+	};
+}
