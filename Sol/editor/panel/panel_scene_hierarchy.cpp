21f20962d470137caf19f7c5a4b8e14808edb56e
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-24 21:41:05 +0100

    Scene Hierarchy Panel

---
+++ Sol/editor/panel/panel_scene_hierarchy.cpp
@@ -0,0 +1,131 @@
+#include "spch.h"
+#include <nova.h>
+#include "scene_hierarchy.h"
+
+#include "core/app.h"
+
+namespace Sol::Panel {
+
+	void SceneHierarchy::HierarchyTree::insert(Nova::ecs::Entity entity, Nova::Component::Name* name, Nova::Component::Parent* parent) {
+		E e{ entity, name->name, parent };
+		Node<E>* node = nullptr;
+
+		Trees::iterator s{ trees.end() };
+		while (s == trees.cend() && parent) {
+			auto n = new Node<E>({ entity, name->name, parent });
+			if (node)
+				n->children.push_back(node);
+			node = n;
+			trees[std::get<0>(node->data)] = node;
+			entity = { entity.registry(), parent->entity };
+			name = &entity.get<Nova::Component::Name>();
+			parent = entity.try_get<Nova::Component::Parent>();
+			s = trees.find(entity);
+		}
+		if (node) {
+			s->second->children.push_back(node);
+		}
+	}
+	void SceneHierarchy::HierarchyTree::insert(Nova::ecs::Entity entity, Nova::Component::Name* name) {
+		trees[entity] = roots.emplace_back(new Node<E>({ entity, name->name, nullptr }));
+	}
+
+	void SceneHierarchy::gui() {
+
+		auto& app = *static_cast<App*>(&Nova::App());
+		auto& world = app.world;
+		auto& reg = world->registry;
+
+		auto v = reg.view<Nova::Component::Name>(Nova::entt::exclude<Nova::Component::Parent>);
+
+		HierarchyTree tree{ v.size_hint() };
+
+		for (auto&& [e, name] : v.each()) {
+			tree.insert({ reg, e }, &name);
+		}
+		for (auto&& [e, name, parent] : reg.view<Nova::Component::Name, Nova::Component::Parent>().each()) {
+			tree.insert({ reg, e }, &name, &parent);
+		}
+
+		m_selected_valid = false;
+		for (auto& root : tree.traverse()) {
+			traverse(world, reg, *root);
+		}
+		//static constexpr auto context_menu = ;
+		if (Nova::gui::BeginPopupContextWindow("EnttyCntxtMenu", ImGuiPopupFlags_NoOpenOverItems | ImGuiPopupFlags_MouseButtonRight)) {
+			if (Nova::gui::MenuItem("Create New Entity")) {
+				auto e = world->instantiate();
+				e.emplace<Nova::Component::Name>("New Entity");
+				e.emplace<Nova::Component::Transform>();
+			}
+			Nova::gui::EndPopup();
+		}
+
+		if (!m_selected_valid)
+			m_selected = {};
+	}
+
+	void SceneHierarchy::delete_entity_recurse(Nova::ecs::Entity& entity, HierarchyTree::Node<HierarchyTree::E>& node) {
+		if (m_selected == entity) {
+			m_selected = {};
+			m_selected_valid = false;
+		}
+		for (auto& child : node.children) {
+			delete_entity_recurse(Nova::ecs::Entity{ entity.registry(), std::get<0>(child->data) }, *child);
+		}
+
+		entity.remove_all();
+		entity.destroy();
+	}
+
+	void SceneHierarchy::traverse(Nova::Star<Nova::ecs::World>& world, Nova::ecs::Registry& reg, HierarchyTree::Node<HierarchyTree::E>& node) {
+		constexpr static ImGuiTreeNodeFlags s_flags = ImGuiTreeNodeFlags_NavLeftJumpsBackHere
+			| ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick | ImGuiTreeNodeFlags_DefaultOpen
+			| ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_SpanAvailWidth;
+		constexpr static auto context_menu = "EntCntxtMenu";
+		Nova::ecs::Entity entity{ reg, std::get<0>(node.data) };
+		if (entity == m_selected)
+			m_selected_valid = true;
+		auto flags = s_flags;
+		if (node.children.empty())
+			flags |= ImGuiTreeNodeFlags_Leaf;
+		if (entity == m_selected)
+			flags |= ImGuiTreeNodeFlags_Selected;
+
+		auto& name = std::get<1>(node.data);
+		Nova::gui::PushID(std::to_string(entity).c_str());
+		const auto topen = Nova::gui::TreeNodeExV(std::to_string(entity).c_str(), flags, name.c_str(), "");
+		if ((Nova::gui::IsMouseClicked(ImGuiMouseButton_Left) || Nova::gui::IsKeyPressed(Nova::gui::GetKeyIndex(ImGuiKey_Enter))) && Nova::gui::IsItemHovered()) {
+			m_selected = entity;
+			m_selected_valid = true;
+		}
+		if (Nova::gui::BeginPopupContextItem(context_menu)) {
+			if (Nova::gui::MenuItem("Create Child Entity")) {
+				auto e = world->instantiate();
+				e.emplace<Nova::Component::Name>("New Entity");
+				e.emplace<Nova::Component::Transform>();
+				e.emplace<Nova::Component::Parent>(entity);
+			}
+			if (Nova::gui::MenuItem("Delete Entity")) {
+				delete_entity_recurse(entity, node);
+			}
+			Nova::gui::EndPopup();
+		}
+		if (Nova::gui::IsItemHovered()) {
+			constexpr auto button_str = "...";
+			Nova::gui::SameLine(Nova::gui::GetContentRegionMax().x - Nova::gui::CalcTextSize(button_str).x - 2 * Nova::gui::GetStyle().FramePadding.x);
+			if (Nova::gui::SmallButton(button_str) || Nova::gui::IsItemClicked()) {
+				Nova::gui::OpenPopup(context_menu);
+			}
+		}
+		if (topen) {
+			if (!node.children.empty() && entity)
+				for (auto& child : node.traverse())
+					traverse(world, reg, *child);
+			Nova::gui::TreePop();
+		}
+
+		Nova::gui::PopID();
+	}
+
+}
---
+++ Sol/editor/panel/scene_hierarchy.h
@@ -0,0 +1,57 @@
+#pragma once
+#include "spch.h"
+#include <nova.h>
+#include "core/panel.h"
+
+namespace Sol::Panel {
+	class SceneHierarchy : public Panel {
+	public:
+		SceneHierarchy() : Panel("Scene Hierarchy") {}
+		Nova::ecs::Entity& entity() { return m_selected; }
+	protected:
+		virtual void gui() override;
+
+		Nova::ecs::Entity m_selected;
+		bool m_selected_valid = false;
+
+		struct HierarchyTree {
+			template<typename T>
+			struct Node {
+				Node(const T& data) : data(data), children() {}
+				T data;
+				std::vector<Node<T>*> children;
+				std::vector<Node<T>*>& traverse() {
+					std::sort(children.begin(), children.end(), [](Node<T>*& a, Node<T>*& b) -> bool {
+						return std::get<1>(a->data) < std::get<1>(b->data);
+					});
+					return children;
+				}
+			};
+			using E = std::tuple<Nova::ecs::Entity::Type, std::string, Nova::Component::Parent*>;
+			using Trees = std::unordered_map<Nova::ecs::Entity::Type, Node<E>*>;
+			void insert(Nova::ecs::Entity entity, Nova::Component::Name* name, Nova::Component::Parent* parent);
+			void insert(Nova::ecs::Entity entity, Nova::Component::Name* name);
+
+			HierarchyTree() = default;
+			HierarchyTree(const size_t& count) : trees(count) {};
+			~HierarchyTree() {
+				for (auto&& [k, v] : trees) {
+					delete v;
+				}
+			}
+
+			std::vector<Node<E>*>& traverse() {
+				std::sort(roots.begin(), roots.end(), [](Node<E>*& a, Node<E>*& b) -> bool {
+					return std::get<1>(a->data) < std::get<1>(b->data);
+				});
+				return roots;
+			}
+
+		protected:
+			Trees trees;
+			std::vector<Node<E>*> roots;
+		};
+		void traverse(Nova::Star<Nova::ecs::World>& world, Nova::ecs::Registry& reg, HierarchyTree::Node<HierarchyTree::E>& node);
+		void delete_entity_recurse(Nova::ecs::Entity& entity, HierarchyTree::Node<HierarchyTree::E>& node);
+	};
+}
