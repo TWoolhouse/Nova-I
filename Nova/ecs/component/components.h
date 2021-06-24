23b737d279c57b91741c79cdc793d26f6783c754
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-06-24 21:25:09 +0100

    component and system header file lists

---
+++ Nova/ecs/component/components.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "name.h"
+#include "parent.h"
+#include "transform.h"
+#include "render.h"
+
+namespace Nova::Component {
+	using namespace Nova::ecs::Components;
+}
---
+++ Nova/ecs/system/systems.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "npch.h"
+#include "../system.h"
+
+#include "sys_render.h"
