e71e06a0d7edb911f81d70b1264fa04505c81556
TWoolhouse <thomasrwoolhouse@gmail.com>
2021-07-30 18:55:16 +0100

    spch

--- Sol/spch.h
+++ Sol/spch.h
@@ -1,23 +1,41 @@
 #pragma once
 
+#include <npch.h>
+#include <type_traits>
+#include <numeric>
+#include <memory>
+#include <array>
+#include <vector>
 #include <string>
+#include <bitset>
+#include <variant>
+#include <forward_list>
+#include <unordered_map>
 #include <utility>
 #include <functional>
-#include <vector>
-#include <array>
-#include <memory>
-#include <cassert>
-#include <variant>
-#include <type_traits>
-#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
 
-// #define NOVA_DEBUG
-// #define NOVA_INTERNAL
-// #define NOVA_RELEASE
+#ifdef SOL_STATIC
+#define SOL_API
+#else
+#ifdef SOL_EXPORT
+#define SOL_API __declspec(dllexport)
+#else
+#define SOL_API __declspec(dllimport)
+#endif // SOL_EXPORT
+#endif // SOL_STATIC
 
- #define SOL_EDITOR
+// #define SOL_EDITOR
 // #define SOL_RUNTIME
 
-#if defined(SOL_EDITOR) && defined(SOL_RUNTIME)
-#error Multiple Sol runtime requirements
+#if (!(defined(SOL_EDITOR) || defined(SOL_RUNTIME)))
+#error No Sol runtime configuration
 #endif
+#if defined(SOL_EDITOR) && defined(SOL_RUNTIME)
+#error Multiple Sol runtime configurations
+#endif
+
+namespace Sol {}
